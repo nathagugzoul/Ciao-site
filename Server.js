da082dccd962a4ec5f5ce9b60b1d83bd50f43735
Nathanaël Azoulay <48614408+nathagugzoul@users.noreply.github.com>
2025-05-06 12:19:02 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Dossier public contenant les fichiers HTML, CSS, JS…
+app.use(express.static(__dirname));
+
+// Route par défaut (index.html)
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur l’API de l’Intelligence Collective !');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
