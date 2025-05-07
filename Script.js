3d323fee233b9a55d82cde8574c90ecc7f5b1fae
Nathanaël Azoulay <48614408+nathagugzoul@users.noreply.github.com>
2025-05-07 08:19:32 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+<input type="text" id="userInput" placeholder="Pose ta question..." />
+<button onclick="askOpenAI()">Soumettre à l'IC</button>
+<div id="responseOutput"></div>
+
+<script src="script.js"></script>
